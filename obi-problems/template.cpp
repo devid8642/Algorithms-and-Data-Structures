98cbabeeeafe5595e4e2e988b08197b30208e980
Devid <nnick4673@gmail.com>
2021-06-04 18:28:52 -0300

    Template file for troubleshooting added.

---
+++ obi-problems/template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+typedef long long int lli;
+#define pii pair<int, int>;
+#define ff first
+#define ss second
+#define pb push_back
+#define mk make_pair
+
+using namespace std;
+
+int main() {
+
+	return 0;
+}
