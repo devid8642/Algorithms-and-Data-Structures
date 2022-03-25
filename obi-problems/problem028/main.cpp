53a98613731f83d61f4ab3f17809329393f3e1c2
Devid <nnick4673@gmail.com>
2022-03-25 14:13:28 -0300

    Simplified data structure

--- obi-problems/problem028/main.cpp
+++ obi-problems/problem028/main.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main() {
     int n, cont = 0;
-    vector<pair<int, char>> botas_d;
-    vector<pair<int, char>> botas_e;
+    vector<int> botas_d;
+    vector<int> botas_e;
 
     cin >> n;
 
@@ -16,28 +16,30 @@ int main() {
         cin >> x >> c;
 
         if (c == 'D')
-            botas_d.push_back({x, c});
+            botas_d.push_back(x);
         else
-            botas_e.push_back({x, c});
+            botas_e.push_back(x);
     }
 
     sort(botas_d.begin(), botas_d.end());
     sort(botas_e.begin(), botas_e.end());
 
     for (int i = 0; i < n/2; i++) {
-        int tam = botas_d[i].first;
+        int tam = botas_d[i];
 
         for (int j = 0; j < (int)botas_e.size(); j++) {
-            if (tam == botas_e[j].first) {
+            cout << botas_e[j] << " ";
+            if (tam == botas_e[j]) {
                 cont++;
                 botas_e.erase(botas_e.begin()+j, botas_e.begin()+j);
                 break;
             }
             else {
-                if (botas_e[j].first > tam)
+                if (botas_e[j] > tam)
                     break;
             }
         }
+        cout << endl;
     }
 
     cout << cont << endl;
