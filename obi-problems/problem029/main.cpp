4b6e7ad5464c6f1e9b2cf5cfc9175ff463e6e654
Devid <nnick4673@gmail.com>
2022-03-31 19:07:54 -0300

    Problem 29 solution submeted

--- obi-problems/problem029/main.cpp
+++ obi-problems/problem029/main.cpp
@@ -3,6 +3,32 @@
 using namespace std;
 
 int main() {
+    int ia, ib, fa, fb, cont = 0;
+
+    cin >> ia >> ib >> fa >> fb;
+
+    while (ia != fa || ib != fb) {
+        if (ib != fb) {
+            if (ib == 0)
+                ib = 1;
+            else
+                ib = 0;
+            if (ia == 0)
+                ia = 1;
+            else
+                ia = 0;
+            cont++;
+        }
+        else if (ia != fa) {
+            if (ia == 0)
+                ia = 1;
+            else
+                ia = 0;
+            cont++;
+        }
+    }
+
+    cout << cont << endl;
 
     return 0;
 
