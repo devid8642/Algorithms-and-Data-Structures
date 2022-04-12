98f8b34de0a0ddcec7f741bd5c539bccfe6a3f05
Devid <nnick4673@gmail.com>
2022-04-12 17:42:39 -0300

    Problem solution

--- problems/problem019/main.cpp
+++ problems/problem019/main.cpp
@@ -4,22 +4,24 @@ using namespace std;
 
 int main() {
     int n, d;
-    vector<stack<int>> nums;
+    vector<vector<int>> nums;
     vector<int> ks;
+    vector<int> ds;
 
     cin >> n >> d;
 
     while (n != 0 && d != 0) {
         string x;
-        stack<int> num;
+        vector<int> num;
         int k =  n - d;
         ks.push_back(k);
+        ds.push_back(d);
 
         cin >> x;
 
         for (int i = 0; i < (int)x.size(); i++) {
             int k = x[i] - 48;
-            num.push(k);
+            num.push_back(k);
         }
 
         nums.push_back(num);
@@ -29,40 +31,23 @@ int main() {
     }
 
     for (int i = 0; i < (int)nums.size(); i++) {
-        stack<int> num = nums[i];
-        stack<int> n_num;
+        vector<int> num = nums[i];
+        vector<int> n_num;
         int k = ks[i];
-        int j = 0;
+        int d = ds[i];
+        int l = 0;
 
-        while (num.size() != 0) {
-            if (j == 0) {
-                n_num.push(num.top());
-                num.pop();
-                j++;
+        for (int j = 0; j < (int)num.size(); j++) {
+            while (n_num.size() > 0 && num[j] > n_num.back() && l < d) {
+                n_num.pop_back();
+                l++;
             }
-            else {
-                if (num.top() > n_num.top()) {
-                    n_num.pop();
-                    n_num.push(num.top());
-                    num.pop();
-                }
-                else {
-                    if (n_num.size() == k) {
-                        num.pop();
-                        continue;
-                    }
-                    else {
-                        n_num.push(num.top());
-                        num.pop();
-                    }
-                }
-            }
+            if (n_num.size() < k)
+                n_num.push_back(num[j]);
         }
 
-        while (n_num.size() != 0) {
-            cout << n_num.top();
-            n_num.pop();
-        }
+        for (int j = 0; j < (int)n_num.size(); j++)
+            cout << n_num[j];
         cout << endl;
     }
 
