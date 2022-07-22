80a79941c4998e121c6b63e4e37500b8878e70de
Devid <nnick4673@gmail.com>
2022-07-22 11:14:01 -0300

    solução do problema 236

---
+++ problems/problem236/main.cpp
@@ -0,0 +1,23 @@
+#include "bits/stdc++.h"
+
+#define pb push_back
+#define mk make_pair
+#define fi first
+#define se second
+
+using namespace std;
+
+int main() {
+    int w;
+
+    cin >> w;
+
+    if (w % 2 == 0 && w > 2)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+
+
+    return 0;
+
+}
