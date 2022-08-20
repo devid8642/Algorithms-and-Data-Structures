5e85e0840a7e7868ca8328d25ffbc04b6b7e4b2f
Devid <nnick4673@gmail.com>
2022-08-20 08:38:50 -0300

    Solução do problema 254 (codeforces)

---
+++ problems/problem254/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+     
+using namespace std;
+     
+int main() {
+    int n, m;
+    double result;
+        
+    cin >> n >> m;
+        
+    result = (n * m)/2;
+        
+    cout << ceil(result) << endl;
+     
+    return 0;
+}
