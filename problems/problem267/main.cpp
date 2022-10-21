c3aaff2f654e2b26e6e65db38c9f0a7587a5557f
devid <nnick4673@gmail.com>
2022-10-21 11:11:29 -0300

    Solução do problema 267 - Neps Academy

---
+++ problems/problem267/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int v[3];
+    
+    cin >> v[0] >> v[1] >> v[2];
+    
+    sort(v, v + 3);
+    
+    cout << v[1] << endl;
+
+    return 0;
+}
