347326ec845c1e1e8217cae508adf673c746b16c
devid <nnick4673@gmail.com>
2023-11-06 12:05:22 -0300

    implementation of a circular linked list

---
+++ algo_ds/python_algo_ds/circular_linked_list/circular_linked_list.py
@@ -0,0 +1,52 @@
+""" Implementation of a circular linked list """
+from __future__ import annotations
+from collections.abc import Iterable
+from typing import Any
+from linked_list.linked_list import LinkedList
+from linked_list.linked_list import Node
+
+
+class CircularLinkedList(LinkedList):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def __iter__(self) -> Iterable:
+        current = self._head
+
+        while current:
+            yield current.data
+            current = current.next
+            if current == self._head:
+                break
+
+    def append(self, value: Any) -> None:
+        new_node = Node(value)
+
+        if not self._head:
+            self._head = self._last = new_node
+            self._last.next = self._head
+        else:
+            self._last.next = new_node
+            self._last = new_node
+            self._last.next = self._head
+
+        self._len += 1
+
+    def disappend(self) -> None:
+        self._last = self._get_node(len(self) - 2)
+        self._last.next = self._head
+        self._len -= 1
+
+    def search(self, value: Any) -> int | None:
+        current = self._head
+        pos = 0
+
+        while current:
+            if current.data == value:
+                return pos
+            current = current.next
+            pos += 1
+            if current == self._head:
+                break
+
+        return None
