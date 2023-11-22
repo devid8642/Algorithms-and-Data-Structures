dd8eb0caa5af7490c42198872d343eefa1cc380b
devid <nnick4673@gmail.com>
2023-11-22 15:42:28 -0300

    Selection sort implementation

---
+++ algo_ds/python_algo_ds/algorithms/selection_sort.py
@@ -0,0 +1,27 @@
+""" Selection sort implementation """
+from __future__ import annotations
+from typing import Any
+from utils.test_sort import TestSortAlgorithms
+import unittest
+
+
+def selection_sort(arr: list[Any]) -> list[Any]:
+    for i in range(0, len(arr) - 1):
+        pos_minor = i
+
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[pos_minor]:
+                pos_minor = j
+
+        arr[i], arr[pos_minor] = arr[pos_minor], arr[i]
+
+    return arr
+
+
+class TestSelectionSort(TestSortAlgorithms):
+    def setUp(self) -> None:
+        self.sort_function = selection_sort
+
+
+if __name__ == '__main__':
+    unittest.main()
