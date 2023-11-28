3ed324a9682bdb54a3ab29512033484b28b9e9fc
devid <nnick4673@gmail.com>
2023-11-28 15:51:49 -0300

    Generalized tests for search algorithms

--- algo_ds/python_algo_ds/algorithms/linear_search.py
+++ algo_ds/python_algo_ds/algorithms/linear_search.py
@@ -1,6 +1,7 @@
 """ Implementation of a linear search """
 from __future__ import annotations
 from typing import Any
+from utils.test_search import TestSearchAlgorithms
 import unittest
 
 
@@ -11,15 +12,10 @@ def linear_search(arr: list[Any], target: Any) -> int | None:
     return None
 
 
-class TestLinearSearch(unittest.TestCase):
+class TestLinearSearch(TestSearchAlgorithms):
     def setUp(self) -> None:
-        self.arr = [31, 41, 59, 26, 41, 58]
-
-    def test_with_an_existing_element(self) -> None:
-        self.assertEqual(linear_search(self.arr, 59), 2)
-
-    def test_with_an_not_existing_element(self) -> None:
-        self.assertEqual(linear_search(self.arr, 10), None)
+        super().setUp()
+        self.search_algorithm = linear_search
 
 
 if __name__ == '__main__':
---
+++ algo_ds/python_algo_ds/algorithms/utils/test_search.py
@@ -0,0 +1,29 @@
+""" Tests for search algorithms """
+import unittest
+from typing import Any
+
+
+def generic_search_algorithm(array: list[Any], value: Any) -> int | None:
+    try:
+        index = array.index(value)
+    except ValueError:
+        return None
+    return index
+
+
+class TestSearchAlgorithms(unittest.TestCase):
+    def setUp(self) -> None:
+        self.arr = [31, 41, 59, 26, 41, 58]
+        self.search_algorithm = generic_search_algorithm
+
+    def test_with_an_existing_element(self) -> None:
+        self.assertEqual(self.search_algorithm(self.arr, 41), self.arr.index(41))
+        self.assertEqual(self.search_algorithm(self.arr, 59), self.arr.index(59))
+        self.assertEqual(self.search_algorithm(self.arr, 26), self.arr.index(26))
+
+    def test_with_an_not_existing_element(self) -> None:
+        self.assertEqual(self.search_algorithm(self.arr, 10), None)
+
+
+if __name__ == '__main__':
+    unittest.main()
