33faf61ff8524a881e9bc7ee63c06b96281dc862
devid <nnick4673@gmail.com>
2023-11-28 15:52:06 -0300

    Binary Search

---
+++ algo_ds/python_algo_ds/algorithms/binary_search.py
@@ -0,0 +1,40 @@
+""" Binary Search implementation """
+from __future__ import annotations
+from typing import Any
+from utils.type_comparable import TypeComparable
+from utils.test_search import TestSearchAlgorithms
+import unittest
+
+
+def binary_search(sorted_collection: list[TypeComparable], value: Any) -> int | None:
+    if sorted_collection != sorted(sorted_collection):
+        raise ValueError('sorted_collection must be sorted in ascending order')
+
+    left = 0
+    right = len(sorted_collection) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if sorted_collection[mid] == value:
+            return mid
+        elif sorted_collection[mid] > value:
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return None
+
+
+class TestBinarySearch(TestSearchAlgorithms):
+    def setUp(self) -> None:
+        super().setUp()
+        self.arr = sorted(self.arr)
+        self.search_algorithm = binary_search
+
+    def test_with_an_unsorted_collection(self) -> None:
+        with self.assertRaises(ValueError):
+            self.search_algorithm([45, 2, 12, 56], 5)
+
+
+if __name__ == '__main__':
+    unittest.main()
