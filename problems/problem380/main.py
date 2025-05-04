ab1d5b4c0da70afaacc38e65bdcabcd4864f4d4b
Dêvid Teófilo <nnick4673@gmail.com>
2025-05-04 16:44:38 -0300

    better solution for p380

--- problems/problem380/main.py
+++ problems/problem380/main.py
@@ -3,7 +3,9 @@
 
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        for i in range(0, len(nums)):
-            for j in range(0, len(nums)):
-                if i != j and nums[i] + nums[j] == target:
-                    return [i, j]
+        seen = {}
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in seen:
+                return [seen[diff], i]
+            seen[num] = i
